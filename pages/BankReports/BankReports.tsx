4f5a02a97a4bf815d1d72f1ab73bcb235b38b623
Dmarcellakbar <dickymarcellino@gmail.com>
2023-01-06 17:07:43 +0700

    Revert "modal add"
    This reverts commit b7d0d56ca46d6502eb398c8af7398dd6b57f4636.

--- pages/BankReports/BankReports.tsx
+++ pages/BankReports/BankReports.tsx
@@ -12,23 +12,13 @@ import {
     Thead,
     Tr,
     HStack,
-    Spacer,
-    ModalOverlay,
-    useDisclosure,
-    Button,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader
+    Spacer
   } from '@chakra-ui/react';
 import {BankData} from './BankData.json';
 import Viewicon from '../../src/icons/search-status.png'
 import Downloadicon from '../../src/icons/document-download.png'
 import Reporticon from '../../src/icons/document-text.png'
 import Image from 'next/image'
-import React from 'react';
 
   interface StatsCardProps {
     title: string;
@@ -37,29 +27,7 @@ import React from 'react';
   
   function StatsCard(props: StatsCardProps) {
     const { title, date } = props;
-    const OverlayOne = () => (
-      <ModalOverlay
-        bg='blackAlpha.300'
-        backdropFilter='blur(10px) hue-rotate(90deg)'
-      />
-    )
-  
-    const OverlayTwo = () => (
-      <ModalOverlay
-        bg='none'
-        backdropFilter='auto'
-        backdropInvert='80%'
-        backdropBlur='2px'
-      />
-    )
-  
-    const { isOpen: isOpenView, onOpen: onOpenView, onClose: onCloseView } = useDisclosure()
-    const { isOpen: isOpenDownload, onOpen: onOpenDownload, onClose: onCloseDownload } = useDisclosure()
-
-    const [overlay, setOverlay] = React.useState(<OverlayOne />)
-    
     return (
-      <>
           <Tbody bgColor={'#1E2432'} >
             <Tr>
               <Td>
@@ -74,73 +42,21 @@ import React from 'react';
               <Td textAlign={'right'}>
                 <HStack >
                 <Spacer />
-                <Box 
-                  w={'24px'} 
-                  cursor={'pointer'} 
-                  mr={'1rem'} 
-                  onClick={() => {
-                  setOverlay(<OverlayOne />)
-                  onOpenView()
-                  }}
-                >
+                <Box w={'24px'} cursor={'pointer'} mr={'1rem'}>
                 <Image src={Viewicon} alt={'view'} loading="lazy"/>
                 </Box>
-                <Box 
-                w={'24px'} 
-                cursor={'pointer'}
-                onClick={() => {
-                  setOverlay(<OverlayTwo />)
-                  onOpenDownload()
-                }}>
+                <Box w={'24px'} cursor={'pointer'}>
                 <Image src={Downloadicon} alt={'download'} loading="lazy"/>
                 </Box>
                 </HStack>
               </Td>
             </Tr>
           </Tbody>
-          {/* modal view */}
-          <Modal isCentered isOpen={isOpenView} onClose={onCloseView}>
-        {overlay}
-        <ModalContent>
-          <ModalHeader>You Want View Report?</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text fontWeight={700}>{title}</Text>
-            <Text>{date}</Text>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onCloseView} colorScheme='blue'>View</Button>
-            <Button onClick={onCloseView}>Close</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-          {/* modal download */}
-          <Modal isCentered isOpen={isOpenDownload} onClose={onCloseDownload}>
-        {overlay}
-        <ModalContent>
-          <ModalHeader>Are you sure to download?</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text>Download file</Text>
-            <Text fontWeight={700}>{title}</Text>
-            <Text>{date}</Text>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onCloseDownload} colorScheme='blue'>Download</Button>
-            <Button onClick={onCloseDownload}>Close</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-          </>
     );
   }
   
   export default function BankReports() {
-    
     return (
-      <>
       <Box color={'white'} maxW="auto">
         <chakra.h1
             textAlign={'left'}
@@ -185,6 +101,5 @@ import React from 'react';
         </Stat>
         </SimpleGrid>
       </Box>
-      </>
     );
   }
